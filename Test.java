e72dc0fc9ee0a527042a943185463cc625c3eb6d
tirupatirao233 <47588932+tirupatirao233@users.noreply.github.com>
2019-06-08 19:52:55 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,6 +1,5 @@
-/* This is a simple Java program. 
-FileName : "HelloWorld.java". */
-Test
+
+class Test
 { 
 	// Your program begins with a call to main(). 
 	// Prints "Hello, World" to the terminal window. 
